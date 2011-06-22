e292c17e170d001ecdec5cb20bd040789ff857ee
Torsten Fehre <post@feson.de>
2011-06-22 02:04:11 +0200

    Auswertung fertig

--- auswertung/convert_to_csv.php
+++ auswertung/convert_to_csv.php
@@ -17,13 +17,16 @@ fputcsv($outfile, $heads);
 $queryPerson = mysql_query("SELECT * FROM t_persons") or die(mysql_error());
 while($rowPerson = mysql_fetch_assoc($queryPerson)) {
 	$line = array(
-		$rowPerson['id'], $rowPerson['bogen_id'
-	);
-	$line[] = $rowP
+		$rowPerson['id'], $rowPerson['bogen_id'], $rowPerson['created_at'], $rowPerson['submitted_at']);
+	foreach($properties as $prop)
+		$line[] = query_property($rowPerson['id'], $prop);
+	fputcsv($outfile, $line);
 }
 
+fclose($outfile);
+
 function query_property($person_id, $property) {
-	$query = mysql_query("SELECT value FROM t_person_properties WHERE person_id = ".intval($person_id)." AND property='".mysql_real_escape_string($property).' LIMIT 1');
+	$query = mysql_query("SELECT value FROM t_person_properties WHERE person_id = ".intval($person_id)." AND property='".mysql_real_escape_string($property)."' LIMIT 1") or die(mysql_error());
 	$row = mysql_fetch_row($query);
 	if (!$row)
 		return null;
